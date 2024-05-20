0f838f5e612b9bd577688a0a8a00bf95c080c6a6
nxtkallax <areer@outlook.jp>
2024-05-20 12:01:07 +0000

    na hömma, was hast du jetzt wieder angestellt?

--- spassig/zufallsgenerator/scripts.js
+++ spassig/zufallsgenerator/scripts.js
@@ -1,17 +1,21 @@
-function zufallsAktion() {
-    // Definiere die Prozentwahrscheinlichkeit (z.B. 30%)
-    var wahrscheinlichkeit = 10;
-
-    // Generiere eine Zufallszahl zwischen 1 und 100
-    var zufallszahl = Math.floor(Math.random() * 100) + 1;
-
-    // Überprüfe, ob die Zufallszahl kleiner oder gleich der Prozentwahrscheinlichkeit ist
-    if (zufallszahl <= wahrscheinlichkeit) {
+function zufallsAktion(min = 1, max = 5) {
+    var zufallszahl = Math.floor(Math.random() * (max - min + 1)) + min;
+    if (zufallszahl = 1) {
         window.location.href = 'https://www.youtube.com/watch?v=vES5N7ouo8A';
-
-    } else {
-        window.location.href = 'https://www.youtube.com/watch?v=9bZkp7q19f0';
-        // Führe hier eine alternative Aktion aus, wenn die Bedingung nicht erfüllt ist
     }
-}                             
-    
+    if (zufallszahl = 2) {
+        window.location.href = 'https://www.youtube.com/watch?v=9bZkp7q19f0';
+    }
+    if (zufallszahl = 3) {
+        window.location.href = 'https://www.youtube.com/watch?v=gT2wY0DjYGo';
+    }
+    if (zufallszahl = 4) {
+        window.location.href = 'https://www.youtube.com/watch?v=cbB3iGRHtqA';
+    }
+    if (zufallszahl = 5) {
+        window.location.href = 'https://www.youtube.com/watch?v=1o69aqubNcMo';
+    } else {
+        console.log("na hömma, was hast du jetzt wieder angestellt?")
+    }
+    console.log("ole ole ole ole")
+}
