30a0ad524713e4b46161476727af793a27ef18ac
nxtkallax <areer@outlook.jp>
2024-05-20 12:05:50 +0000

    samma, was hast du gemacht?

--- spassig/zufallsgenerator/scripts.js
+++ spassig/zufallsgenerator/scripts.js
@@ -1,21 +1,17 @@
 function zufallsAktion(min = 1, max = 5) {
     var zufallszahl = Math.floor(Math.random() * (max - min + 1)) + min;
-    if (zufallszahl = 1) {
+    if (zufallszahl === 1) {
         window.location.href = 'https://www.youtube.com/watch?v=vES5N7ouo8A';
-    }
-    if (zufallszahl = 2) {
+    } else if (zufallszahl === 2) {
         window.location.href = 'https://www.youtube.com/watch?v=9bZkp7q19f0';
-    }
-    if (zufallszahl = 3) {
+    } else if (zufallszahl === 3) {
         window.location.href = 'https://www.youtube.com/watch?v=gT2wY0DjYGo';
-    }
-    if (zufallszahl = 4) {
+    } else if (zufallszahl === 4) {
         window.location.href = 'https://www.youtube.com/watch?v=cbB3iGRHtqA';
-    }
-    if (zufallszahl = 5) {
+    } else if (zufallszahl === 5) {
         window.location.href = 'https://www.youtube.com/watch?v=1o69aqubNcMo';
     } else {
-        console.log("na hömma, was hast du jetzt wieder angestellt?")
+        console.log("na hömma, was hast du jetzt wieder angestellt?");
     }
-    console.log("ole ole ole ole")
+    console.log("ole ole ole ole");
 }
