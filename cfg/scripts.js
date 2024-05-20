cd3c964f47680a6a073d146cb9f289be684fb98c
nxtkallax <areer@outlook.jp>
2024-05-20 12:11:42 +0000

    ole ole ole ole

--- cfg/scripts.js
+++ cfg/scripts.js
@@ -11,14 +11,21 @@ let firstClick = true;
                     zufallsAktion(); 
                 }
             }
-            function zufallsAktion() {
-                var zufallszahl = Math.floor(Math.random() * 100) + 1;
-                if (zufallszahl <= 33) {
+            function zufallsAktion(min = 1, max = 5) {
+                var zufallszahl = Math.floor(Math.random() * (max - min + 1)) + min;
+                if (zufallszahl === 1) {
                     window.location.href = 'https://www.youtube.com/watch?v=vES5N7ouo8A';
-                } else if (zufallszahl >= 66) {
+                } else if (zufallszahl === 2) {
                     window.location.href = 'https://www.youtube.com/watch?v=9bZkp7q19f0';
+                } else if (zufallszahl === 3) {
+                    window.location.href = 'https://www.youtube.com/watch?v=gT2wY0DjYGo';
+                } else if (zufallszahl === 4) {
+                    window.location.href = 'https://www.youtube.com/watch?v=cbB3iGRHtqA';
+                } else if (zufallszahl === 5) {
+                    window.location.href = 'https://www.youtube.com/watch?v=1o69aqubNcMo';
                 } else {
-                    window.location.href = 'https://www.youtube.com/watch?v=gT2wY0DjYGo'
+                    console.log("na hömma, was hast du jetzt wieder angestellt?");
                 }
-            }   
+                console.log("ole ole ole ole");
+            }  
             document.getElementById('Chicken Sakurako').addEventListener('click', ChickenSakurako);
